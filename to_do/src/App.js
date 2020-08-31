cfcce1396fd090587cf19902e76ad19604360a07
tnhubbar <taryn.johnson919@gmail.com>
2020-08-31 19:32:45 -0400

    rendered App to dom

--- to_do/src/App.js
+++ to_do/src/App.js
@@ -4,9 +4,12 @@ import React, { Component } from 'react'
 /* We need to append our app to the DOM */
 export default class App extends Component {
     
+
+
+
     render(){
         return(
-            <h1>Hello World!</h1>
+        <h1>Hi, I'm {this.props.name}</h1>
         )
     }
 
--- to_do/src/index.js
+++ to_do/src/index.js
@@ -2,4 +2,23 @@ import React from 'react'
 import App from './App.js'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App name={"taryn"}/>, document.getElementById("root"))
+
+
+
+
+
+/*
+
+App
+
+RecipeBook store all recipes state: allRecipes: []
+    recipe = responsible for rendering just that recipe info 
+    recipelist = resposnible for creating list items out of recipe collection 
+
+User
+
+
+class => kept track of all its instances
+
+*/
